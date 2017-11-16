4258dd5e82e5289293e4062226ad16657b263acc
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-17 00:24:01 +0700

    test1

--- index.php
+++ index.php
@@ -8,25 +8,47 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'Lffp33UgmurEA06PCo+Aru9LPpZS9heaNnMQim6Uj4ceMzosmD1lgc1l5VMKhnsGfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69Qn1PUTwgr4lsSh5Hf7XCZ8R/5MZqOuynV61EmUT8LNsywdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'f60131c3b22cf492ec86acd0f7eeb0e2';
 
-//Get message from Line API
+// Get POST body content
 $content = file_get_contents('php://input');
-$events=json_decode($content, true);
-
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
 if (!is_null($events['events'])) {
-//Loop through each event foreach($events['events']as $event){
-// Line API send a lot of event type, we interested in message only. if ($event['type'] == 'message') {
-switch($event['message']['type']) {
-    case 'text':
-//Get replyToken
-        $replyToken = $event['replyToken']; //Reply message
-        $respMessage='Hello, your message is '.$event['message']['text'];
-        $httpClient=newCurlHTTPClient($channel_token); 
-        $bot=newLINEBot($httpClient,array('channelSecret'=> $channel_secret)); 
-        $textMessageBuilder=newTextMessageBuilder($respMessage);
-        
-        break;
-    } 
-  }
-$response=$bot->replyMessage($replyToken, $textMessageBuilder); 
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
 echo "OK";
