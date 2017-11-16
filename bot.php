4e08f850de55acf4e35a36f997df58b9fea2d507
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-17 01:15:13 +0700

    Begin Again

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('jf4myrsLqZMwMyra5wuDVfNU2fhADDNoo7851ngpr0V/BFPNAkALW4C4UQ0O/9lyfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69QkieesC8Hh3K2HR5Vi0RHYcAEX4qY4ShCOG/xkYPEmzHAdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('pfJsKyHp/SAvMMdXXZaXsNxKA+YlaN1bGt6aRevlXOj75GMnD4gkPvsF0gDh6hYyfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69QkaPtdtBOTdPPEdPVozICNbwtREYm4L1UAc9g+5oflIFQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'f60131c3b22cf492ec86acd0f7eeb0e2']);
 
 $response = $bot->replyText('<reply token>', 'hello!');
