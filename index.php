dfc0f08a836a80a5e0e8a705cc1a64e5b68bc061
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-17 00:28:17 +0700

    test1

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-require_once('./vendor/autoload.php');
-// Namespace
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-
 $channel_token = 'Lffp33UgmurEA06PCo+Aru9LPpZS9heaNnMQim6Uj4ceMzosmD1lgc1l5VMKhnsGfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69Qn1PUTwgr4lsSh5Hf7XCZ8R/5MZqOuynV61EmUT8LNsywdB04t89/1O/w1cDnyilFU=';
-$channel_secret = 'f60131c3b22cf492ec86acd0f7eeb0e2';
 
 // Get POST body content
 $content = file_get_contents('php://input');
