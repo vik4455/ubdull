4aca219a0385e6ba1152b1ed86fb67092da0479e
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-16 23:29:10 +0700

    Initial...

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
 $channel_token = 'xTm6W+W9dDaM5UIZKhFUhdUHExbahJJixWPbl1rG7MjYvpgcf2TukjOM/Ain+7ozfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69QneMTAqDQX7TwW4Dl6gm3+03lZ1E2QSFtRXxUNbCNU5EwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'a112cedb91a3ded4ba9cf92c990c07fb';
-//Get message from Line API
+
+/Get message from Line API
 $content = file_get_contents('php://input');
 $events=json_decode($content, true);
+
 if (!is_null($events['events'])) {
 //Loop through each event foreach($events['events']as $event){
 // Line API send a lot of event type, we interested in message only. if ($event['type'] == 'message') {
