25cbd9eb593d59c0a80ffb36b5e6466264fe1669
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-17 01:11:25 +0700

    Begin Again

--- bot.php
+++ bot.php
@@ -1,46 +1,5 @@
 <?php
-$access_token = 'jf4myrsLqZMwMyra5wuDVfNU2fhADDNoo7851ngpr0V/BFPNAkALW4C4UQ0O/9lyfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69QkieesC8Hh3K2HR5Vi0RHYcAEX4qY4ShCOG/xkYPEmzHAdB04t89/1O/w1cDnyilFU=';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('jf4myrsLqZMwMyra5wuDVfNU2fhADDNoo7851ngpr0V/BFPNAkALW4C4UQ0O/9lyfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69QkieesC8Hh3K2HR5Vi0RHYcAEX4qY4ShCOG/xkYPEmzHAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'f60131c3b22cf492ec86acd0f7eeb0e2']);
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-echo "OK";
+$response = $bot->replyText('<reply token>', 'hello!');
