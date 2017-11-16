82cd4fb943adcd5096250dcd795c4ecb23e7b1f7
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-16 23:48:01 +0700

    test1

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ switch($event['message']['type']) {
         $textMessageBuilder=newTextMessageBuilder($respMessage);
     } 
   }
-
-$response=$bot->replyMessage($replyToken, $textMessageBuilder); break;
+}
+$response=$bot->replyMessage($replyToken, $textMessageBuilder); 
+break;
 echo "OK";
