72c2cdea7ae3c08f65599c22c8e2786b66dc022b
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-16 23:45:04 +0700

    test1

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'Lffp33UgmurEA06PCo+Aru9LPpZS9heaNnMQim6Uj4ceMzosmD1lgc1l5VMKhnsGfhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69Qn1PUTwgr4lsSh5Hf7XCZ8R/5MZqOuynV61EmUT8LNsywdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'f60131c3b22cf492ec86acd0f7eeb0e2';
 
-/Get message from Line API
+//Get message from Line API
 $content = file_get_contents('php://input');
 $events=json_decode($content, true);
 
