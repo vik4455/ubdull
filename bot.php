5021fedf82beafbcbd0efd1fffbfcd7d8188b52d
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-17 06:22:19 +0700

    Begin Again

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message') {
 			switch($event['message']['type']) {
                 case 'text':
                     //Get replyToken
