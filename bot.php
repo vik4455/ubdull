695b57088c8e7dde726ccadc088d7141f6c62207
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-18 06:54:03 +0700

    Ubdull Test4

--- bot.php
+++ bot.php
@@ -4,6 +4,8 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
 
 $channel_token = 'wg5krRjKGZ3LnV109YBgOLOJ0f/cdg2ZkXHUjW8G9GLbDPhG8RasvXp6IXOq2gk0fhU083AzutOM0pJbyVHb9L5bewHM5145QWTRz5a69QnhHQFGR1LmLYPzkLv6oJz/0493BO+moa6fBWN76U3XMgdB04t89/1O/w1cDnyilFU=';
@@ -23,22 +25,36 @@ if (!is_null($events['events'])) {
                     //Get replyToken
                     $replyToken = $event['replyToken']; //Reply message
                     if($event['message']['text']=='ubdull'){
-                        $respMessage='เรามารวยไปด้วยกันนะ พิมพ์ ทีเด็ด1,2,3';   
+                        $respMessage='เรามารวยไปด้วยกันนะ พิมพ์ ทีเด็ด1,2,3';
+                        $textMessageBuilder=new TextMessageBuilder($respMessage);
                     }else if($event['message']['text']=='ทีเด็ด1'){
                         $respMessage='เชฟฟิลด์ ยูไนเต็ด ต่อ ครึ่งลูกครับ แม้จะเป็นเกมส์เยือนแต่เบอร์ตัน อัลเบี้ยนเล่นในบ้านฟอร์มไม่ดีเลย กัดฟันต่อดาบคู่ครับ อับดุลมั่นจายย';
+                        $textMessageBuilder=new TextMessageBuilder($respMessage);
                     }else if($event['message']['text']=='ทีเด็ด2'){
                         $respMessage='สูง 2.5 บุนเดสลีกา 2 ไอน์ทรัค เบาร์ชไวน์ พบ บีเลเฟลด์ รั่วด้วยกันทั้งคู่ บีเลเฟลด์ในบ้านกาก ไอน์ทรัคเยือนก็กาก เชื่อว่ายิงกันยับ อับดุลว่าสูงแน่ๆ';
+                        $textMessageBuilder=new TextMessageBuilder($respMessage);
                     }else if($event['message']['text']=='ทีเด็ด3'){
                         $respMessage='เดน บอสช์ เจอ โวเลนดัม ราคาเสมอ โวเลนดัมบ๊วยครับ ฟอร์มย่ำแย่จริงๆ ราคาเสมอลุ้นเดน บอสช์ดีกว่าครับ เชื่ออับดุล เราจะรวยไปด้วยกัน';
+                        $textMessageBuilder=new TextMessageBuilder($respMessage);
                     }else if($event['message']['text']=='บอลสเต็ป'){
                         $respMessage='ต่อเชฟยู - สูงไอน์ทรัค เบราชไวน์ - เดน บอสช์ - วิสล่าคราคอฟ ';
+                        $textMessageBuilder=new TextMessageBuilder($respMessage);
+                    }else if($event['message']['text']=='กะแป๋ง'){
+                        $originalContentUrl = 'https://0.soompi.io/wp-content/uploads/q/3/9w/461510/461510.jpg';
+                        $previewImageUrl = 'https://0.soompi.io/wp-content/uploads/q/3/9w/461510/461510.jpg';
+                        $textMessageBuilder=new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
+                    }else if($event['message']['text']=='กากแป๋ง'){
+                        $packageId = 1; 
+                        $stickerId = 3;
+                        $textMessageBuilder=new StickerMessageBuilder($packageId, $stickerId);
+                        break;
                     }
                     
                     break;
             }
         $httpClient=new CurlHTTPClient($channel_token); 
         $bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret)); 
-        $textMessageBuilder=new TextMessageBuilder($respMessage);
+        
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
         
     }
