290db4017e67c67bfcfccb0f82301bea5089479b
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-27 08:54:05 +0700

    Ubdull Mem Phone

--- bot.php
+++ bot.php
@@ -70,6 +70,7 @@ if (!is_null($events['events'])) {
         $httpClient = new CurlHTTPClient($channel_token);
         $bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
         
+        $textMessageBuilder=new TextMessageBuilder($respMessage);
         $response=$bot->replyMessage($replyToken, $textMessageBuilder);
     }
 }
