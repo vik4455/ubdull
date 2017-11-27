379df10c67901bec4cdf4edfc220a6d9f8d2258d
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-27 09:05:36 +0700

    Ubdull Mem Phone

--- bot.php
+++ bot.php
@@ -30,7 +30,6 @@ if (!is_null($events['events'])) {
                     $user = 'fljlfseofpkpfr';
                     $pass = 'ac9fab1bfcbd77359fb3c7f0a30c571de1e94d13006d1be29aa39e5c978b9182'; 
                     $connection=new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-                    $params = array('tpre'=> $txttel[0], 'tname'=> $txttel[1],'ttel'=> $txttel[2],);
                     
                     $sql=sprintf("SELECT * FROM com4_6_phone WHERE name ='".$txttel[1]."'");
                     $lname = $connection->query($sql);
@@ -43,10 +42,11 @@ if (!is_null($events['events'])) {
                         $user = 'fljlfseofpkpfr';
                         $pass = 'ac9fab1bfcbd77359fb3c7f0a30c571de1e94d13006d1be29aa39e5c978b9182'; 
                         $connection=new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-                        
-                        $statement=$connection->prepare("INSERT INTO com4_6_phone (name,mobile)VALUES(:tname,:ttel)");
+                        $params = array('tpre'=> $txttel[0], 'tname'=> $txttel[1],'ttel'=> $txttel[2],);
+                        $statement=$connection->prepare("INSERT INTO com4_6_phone (name,mobile) VALUES(:tname,:ttel)");
                         $result = $statement->execute($params);
                         $respMessage='จำเบอร์ '.$txttel[1].' เรียบร้อยแล้ว';
+                        
                         }else{
                         $respMessage='เบอร์ '.$txttel[1].' จำไปแล้วครับ พิมพ์ เบอร์,ชื่อคนที่ต้องการ เพื่อดูเบอร์';    
                         }   
