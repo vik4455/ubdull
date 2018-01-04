4f850676434faa5a7a5e44086f1351227133ffb7
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2018-01-04 20:20:01 +0700

    Ubdull Update

--- bot.php
+++ bot.php
@@ -19,36 +19,65 @@ $events=json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-        $replyToken = $event['replyToken']; 
-		
-        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-            $host = 'ec2-54-163-255-181.compute-1.amazonaws.com';
-            $dbname = 'dcoh0blsle9i6l'; 
-            $user = 'fljlfseofpkpfr';
-            $pass = 'ac9fab1bfcbd77359fb3c7f0a30c571de1e94d13006d1be29aa39e5c978b9182'; 
-            $connection=new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-            
-            switch($event['message']['text']) {
-            case 'ที่อยู่': $respMessage = 'หจก. พิน ไอที โซลูชั่น 
-445/5 ถ.พายัพทิศ ต.ในเมือง 
-อ.เมือง จ.นครราชสีมา 30000';
-            break;
-            case 'โทร': $respMessage = 'เบอร์โทร 
- 090-9539419
- 097-0045666';
-            break;
-            case 'ราคา': $respMessage = 'ทำเว็บไซต์ เริ่มต้นที่ 5,000 บาท 
-เขียนโปรแกรมเริ่มที่ 8,500 บาท 
-Line Bot แบบผม 5,000 บาท ครับ';
-            break;
-            default : $respMessage = 'สวัสดีครับ วันนี้เราขอแนะนำบริการของ Pin IT Solution';
-            break;
-            }
-            
-        }//if event
-        
+        include 'include/connect.php';
         $httpClient = new CurlHTTPClient($channel_token);
         $bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
+        $grp = $event['source']['groupId'];
+        $user = $event['source']['userId'];
+        $replyToken = $event['replyToken'];
+        
+        $ins = $conn->query('SELECT MAX(idmember) AS mm FROM member');
+                $mm = $ins->fetch_assoc();
+                $nid = str_pad($mm['mm']+1,3,"0",STR_PAD_LEFT);
+		$res = $bot->getProfile($user);
+        if ($res->isSucceeded()) {
+            $profile = $res->getJSONDecodedBody();
+            $displayName = $profile['displayName'];
+        }
+        if ($event['type'] == 'message') {
+            switch($event['message']['type']) {
+                case 'text':
+                $ct = $event['message']['text'];
+                
+                $txt =explode(',', $ct);
+                $dt = date('Y-m-d');
+                
+                if(($txt[0]=="rg")&&(strlen($txt[1])==13)&&(is_numeric($txt[1]))){
+                $add_user = $conn->query('INSERT INTO 
+                            member (idmember,mem_name,mem_id,mem_card,mem_status,date_regis) 
+                            VALUES ("'.$nid.'","'.$displayName.'","'.$user.'","'.$txt[1].'","11","'.$dt.'")');
+                            if (!$add_user) {
+                                die('Add Member : '.$conn->error);
+                            }
+                $respMessage= "ลงทะเบียนสมาชิก ".$ni." ชื่อ : ".$displayName." ".$txt[1]." User ID : ".$user." วันที่ : ".$dt;
+                }else{
+                $respMessage= "format ลงทะเบียนคือ rg,(เลขบัตรประชาชน)";
+                }
+                break;
+                case 'image':
+                $fileID = $event['message']['id']; 
+                $response = $bot->getMessageContent($fileID); 
+                $fileName = md5(date('Y-m-d')).'.jpg'; 
+                if ($response->isSucceeded()) {         // Create file.         
+                    $file = fopen($fileName, 'w');         
+                    fwrite($file, $response->getRawBody());
+                    $upd_user = $conn->query('UPDATE member SET mem_cardimg = "'.$fileName.'"');
+                            if (!$upd_user) {
+                                die('Add Member : '.$conn->error);
+                            }
+                }else {
+                    error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
+                }
+                $respMessage='บันทึกรูปบัตรประชาชน';
+                break;
+                case 'location':
+                $respMessage='สถานที่';
+                break;
+                case 'audio':
+                $respMessage='เสียง';
+                break;
+            }   
+        }
         
         $textMessageBuilder=new TextMessageBuilder($respMessage);
         $response=$bot->replyMessage($replyToken, $textMessageBuilder);
